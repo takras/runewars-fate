f92a0ab81cf616b052d706ca78644e37e665c779
Nelson Menezes <nemzes@gmail.com>
2022-02-16 13:54:27 +0100

    Better 3d

--- components/card-results/index.tsx
+++ components/card-results/index.tsx
@@ -1,4 +1,6 @@
-import { CardType, Shapes } from "../../pages/api/types";
+import hash from "hash-sum";
+
+import { CardType } from "../../pages/api/types";
 import ShapeResults from "../shape-results";
 import styles from "./index.module.css";
 
@@ -13,8 +15,10 @@ const CardResults = ({
   hexagon: Record<CardType, number>;
   circle: Record<CardType, number>;
 }) => {
+  // Generate unique key to force a re-render by React; triggering the CSS animation for the card
+  const keyFromValues = hash({ triangle, rectangle, hexagon, circle });
   return (
-    <section className={styles.cardResults}>
+    <section key={keyFromValues} className={styles.cardResults}>
       <h2 className="sr-only">Results</h2>
       <ShapeResults results={triangle} shape="triangle" />
       <ShapeResults results={rectangle} shape="rectangle" />
